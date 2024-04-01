2d6f3618c0e9947be585b612073a760f8818ce55
GobbinKorneel <korneel.gobbin@gmail.com>
2024-04-01 18:08:34 +0200

    lager en vorige klas, school

--- Ongoing/OKI_Jaartotalen_laatste login_afwezigheden.sql
+++ Ongoing/OKI_Jaartotalen_laatste login_afwezigheden.sql
@@ -174,6 +174,7 @@ verder as(
 select 
 	instellingen.ID as IS_ID,
 	klassen.KL_CODE,
+	klassen.ID as KL_ID,
 	schooljaren.ID as SJ_ID,
 	IngVakken.ID as IV_ID,
 	Leerlingen.ID as LL_ID2,
@@ -427,8 +428,110 @@ lkr as (
 	group by SJ.ID, ins.ID, LSD_KLASCODE, kl.KL_CODE, kl1.KL_CODE, IV.ID, IV.IV_NAAMGEBRUIKER
 
 
-)
+),
+VL as
+(
+SELECT SCHOLen.ID as SC_ID,
+  Scholen.SC_NAAM,
+  Scholen.SC_INSTELLINGSNUMMER,
+  Gemeenten.GM_DEELGEMEENTE,
+  Gemeenten.GM_POSTNUMMER as GM_POSTCODE,
+  Klassen.ID as KL_ID,
+  Klassen.KL_OMSCHRIJVING,
+  Schooljaren.SJ_OMSCHRIJVING,
+  Schooljaren.SJ_GELDIGVAN,
+  Schooljaren.SJ_GELDIGTOT,
+  Schooljaren.ID as SJ_ID,
+  leerjaar.P_OMSCHRIJVING as leerjaar,
+  Leerlingen.ID as LL_ID,
+  Leerlingen.LL_NAAM,
+  Leerlingen.LL_VOORNAAM,
+  Loopbanen.LB_VAN,
+  Loopbanen.LB_TOT
+from Loopbanen
+  LEFT JOIN INUIT ON INUIT.ID = Loopbanen.LB_INUIT_FK
+  LEFT JOIN Leerlingen ON Leerlingen.ID = INUIT.IU_LEERLING_FK
+  LEFT JOIN Scholen ON Scholen.ID = INUIT.IU_SCHOOL_FK
+  LEFT JOIN Klasgroepen ON Klasgroepen.ID = Loopbanen.LB_KLASGROEP_FK
+  LEFT JOIN Klassen ON Klassen.ID = Klasgroepen.KG_KLAS_FK
+  LEFT JOIN Schooljaren ON Schooljaren.ID = Klassen.KL_SCHOOLJAAR_FK
+  LEFT JOIN ParmTabs leerjaar ON leerjaar.ID = Loopbanen.LB_LEERJAAR_FKP
+  LEFT JOIN Gemeenten ON Gemeenten.ID = Scholen.SC_GEMEENTE_FK
+    where Loopbanen.LB_TOT <= inuit.IU_DATUMUITSCHRIJVING   
+  and (Klassen.KL_OMSCHRIJVING <> 'Internaat' or Klassen.KL_OMSCHRIJVING is null)
+  and Scholen.SC_NAAM not like 'Internaat 1 Tielt' 
+  and Scholen.SC_NAAM not like 'Internaat 2 Tielt'  
+),
 
+
+VL1 as 
+(SELECT vl.SC_ID,
+  vl.SC_NAAM,
+  vl.SC_INSTELLINGSNUMMER,
+  vl.GM_DEELGEMEENTE,
+  vl.GM_POSTCODE,
+  vl.KL_ID,
+  vl.KL_OMSCHRIJVING,
+  vl.SJ_OMSCHRIJVING,
+  vl.SJ_GELDIGVAN,
+  vl.SJ_GELDIGTOT,
+  vl.SJ_ID,
+  vl.leerjaar,
+  vl.LL_ID,
+  vl.LL_NAAM,
+  vl.LL_VOORNAAM,
+  vl.LB_VAN,
+  vl.LB_TOT,
+  ROW_NUMBER() over (Partition by vl.LL_ID order by vl.LB_VAN asc) as [Index],
+  ROW_NUMBER() over (Partition by vl.LL_ID order by vl.LB_VAN asc) -1 as [Index - 1]
+FROM VL
+  ),
+
+
+VL2 as (
+  select 
+  *,
+  CONVERT(nvarchar(50),VL1.LL_ID) + '_' + CONVERT(nvarchar(50),[Index]) as [Merge],
+  CONVERT(nvarchar(50),VL1.LL_ID) + '_' + CONVERT(nvarchar(50),[Index - 1]) as [Merge - 1]
+  from VL1
+),
+VL3 as (
+
+select 
+	huidig.LL_ID,
+--	vorig.SC_ID, --
+
+	huidig.KL_ID,
+--	vorig.KL_ID, --
+
+	huidig.SJ_ID,
+--	vorig.SJ_ID, --
+
+--	huidig.SC_NAAM as [Huidige school], --
+	vorig.SC_NAAM as [Vorige school],
+
+--	huidig.KL_OMSCHRIJVING as [Huidige klas], --
+	vorig.KL_OMSCHRIJVING as [Vorige klas],
+
+	vorig.LB_TOT as [Vorige tot],
+
+	lagere.SC_NAAM as [Lagere school],
+
+	lagere.leerjaar as Lager,
+
+
+
+	iif(huidig.SJ_ID = vorig.SJ_ID and huidig.SC_ID = vorig.SC_ID, 1, 0) as [Verandering klas tijdens schooljaar],
+	iif(huidig.SJ_ID = vorig.SJ_ID and (huidig.SC_ID != vorig.SC_ID or vorig.SC_ID is null), 1, 0) as [Zij-instromer school tijdens schooljaar],
+	iif(huidig.SJ_ID = vorig.SJ_ID or huidig.sC_ID = vorig.SC_ID, 0, 1) as [Instroom in school bij begin schooljaar]
+	
+
+
+from vl2 huidig
+left join vl2 vorig on huidig.[Merge - 1] = vorig.[Merge]
+left join vl2 lagere on huidig.LL_ID = lagere.LL_ID and (lagere.leerjaar = 'Zesde leerjaar' or lagere.leerjaar = 'Vijfde leerjaar' or lagere.leerjaar = 'Onbekend') and lagere.[Index] = 1
+
+)
 
 
 
@@ -453,13 +556,27 @@ lkr.[Leerkracht(en) gekoppeld aan vak],
 lkr.[Getuigschrift(en) ],
 lkr.[Plaats(en) uitreiken ],
 lkr.[Datum(s) uitreiken ],
-lkr.[Niveau('s) ]
+lkr.[Niveau('s) ],
+
+vl3.[Instroom in school bij begin schooljaar],
+vl3.[Verandering klas tijdens schooljaar],
+vl3.[Zij-instromer school tijdens schooljaar],
+vl3.[Vorige klas],
+vl3.[Vorige school],
+vl3.[Vorige tot],
+vl3.[Lagere school],
+vl3.Lager as [Laatste leerjaar in lagere school]
+
 
 
 
 from verder
 left join adressen on LL_ID2 = adressen.LA_LEERLING_FK
 left join lkr on lkr.IS_ID = verder.IS_ID and lkr.IV_ID = verder.IV_ID and lkr.Klascode = verder.KL_CODE and lkr.SJ_ID = verder.SJ_ID
+
+left join vl3 on LL_ID2 = vl3.LL_ID and vl3.SJ_ID = verder.SJ_ID and vl3.KL_ID = verder.KL_ID
+
+
 
 order by LL_NAAM, LL_VOORNAAM, [Datum uitreiking]
 
